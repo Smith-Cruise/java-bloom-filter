e7310814b05559aae138b70e1e0b06ca5af6dd27
Smith Cruise <chendingchao1@126.com>
2021-03-07 15:01:05 +0800

    finish

---
+++ src/main/java/org/inlighting/BloomFilterIO.java
@@ -0,0 +1,29 @@
+package org.inlighting;
+
+
+import org.inlighting.proto.BloomFilterProtos;
+
+public class BloomFilterIO {
+
+    public static void serialize(BloomFilterProtos.BloomFilter.Builder builder, BloomFilter bloomFilter) {
+        builder.clear();
+        builder.setNumHashFunctions(bloomFilter.getNumHashFunctions());
+        long[] bitset = bloomFilter.getBitSet();
+        for (long l : bitset) {
+            builder.addBitset(l);
+        }
+    }
+
+    public static BloomFilter deserialize(BloomFilterProtos.BloomFilter bloomFilterProtos) {
+        if (bloomFilterProtos == null) {
+            return null;
+        }
+
+        int numFuncs = bloomFilterProtos.getNumHashFunctions();
+        long[] bitset = new long[bloomFilterProtos.getBitsetCount()];
+        for (int i=0; i<bitset.length; i++) {
+            bitset[i] = bloomFilterProtos.getBitset(i);
+        }
+        return new BloomFilter(bitset, numFuncs);
+    }
+}
